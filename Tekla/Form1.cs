f644bcc5c9ce505e44e3e55090d559dd769519dc
ramin <ramin@DESKTOP-JMLFCHG>
2017-11-28 23:05:30 +0330

    from home ramin azadi comments.

--- Tekla/Form1.cs
+++ Tekla/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
 
 namespace Tekla
 {
@@ -14,11 +9,42 @@ namespace Tekla
     {
         public Form1()
         {
-            InitializeComponent();
+            var model = new Model();  //получаем доступ к модели
+            var CurrentDrawingHandler = new DrawingHandler();  //получаем доступ к чертежам
+
+            //если подключение прошло успешно
+            if (model.GetConnectionStatus() &&
+                CurrentDrawingHandler.GetConnectionStatus())
+            {
+                InitializeComponent();
+            }
+            else
+                MessageBox.Show("Tekla Structures must be opened!");
+          
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
+
+
+            Model m = new Model();
+            Beam b = new Beam();
+
+            b.Class = "4";
+            b.Name = "myBeam";
+            b.Profile.ProfileString = "UB406*140*46";
+            b.StartPoint = new Structures.Geometry3d.Point(0,0,0);
+            b.EndPoint = new Structures.Geometry3d.Point(10000,0,0);
+            b.Material.MaterialString = "S275";
+            b.Position.Depth = Position.DepthEnum.MIDDLE;
+            b.Position.Rotation = Position.RotationEnum.TOP;
+            b.Position.Plane = Position.PlaneEnum.MIDDLE;
+
+            b.Insert();
+
+            m.CommitChanges();
+
             label1.Text = "hello";
         }
     }
